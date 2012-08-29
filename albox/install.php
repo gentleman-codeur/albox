300103c5abf6c4d69882a3752851019135b0803c
jedouard <julien@web-in-progress.fr>
2012-08-29 15:03:40 +0200

    ajout d'une classe image

--- albox/install.php
+++ albox/install.php
@@ -1,10 +1,11 @@
 <?php
 require __DIR__.'/config.php';
+require __DIR__.'/SimpleImage.php';
 
 // Vérification si on peut ecrire dans le répertoire.
 if(!is_writable(DIR_IMAGES_MINI))
 {
-	echo 'vous devez avoir votre répertoire accessible en écriture';
+	echo 'vous devez avoir votre répertoire \'/albox/images/\' accessible en écriture';
 	die();
 }
 
@@ -17,15 +18,15 @@ if(!is_dir(DIR_IMAGES_MINI.'/mini/'))
 // Création du répertoire pour les images taille web
 if(!is_dir(DIR_IMAGES_MINI.'/web/'))
 {
-	mkdir(DIR_IMAGES.'/web');
+	mkdir(DIR_IMAGES_MINI.'/web');
 }
 
 // Création des mini images
-crawlDir(DIR_ALBUM);
+crawlDir();
 
-function crawlDir($dir_name){
+function crawlDir($dir_name = null){
 
-	$dir = opendir($dir_name); // on ouvre le contenu du dossier courant
+	$dir = opendir(DIR_ALBUM.$dir_name); // on ouvre le contenu du dossier courant
 	if(!$dir)
 	{
 		return false;
@@ -38,15 +39,23 @@ function crawlDir($dir_name){
 	{
 		if($element != '.' && $element != '..') 
 		{
-			if (!is_dir($dir_name.'/'.$element)) 
+			if (!is_dir(DIR_ALBUM.$dir_name.'/'.$element.'/')) 
 			{
+				
+
+				echo '<br />-'.DIR_ALBUM.$dir_name.'/'.$element;
+				$image = new SimpleImage();
+				$image->load(DIR_ALBUM.$dir_name.'/'.$element);
+				
 				if(!file_exists(DIR_IMAGES_MINI.'/mini/'.$dir_name.'/'.$element))
 				{
-					resize($dir_name.'/'.$element, DIR_IMAGES_MINI.'/mini/'.$dir_name.'/'.$element, 140);
+					$image->resize(153, 116);
+					$image->save(DIR_IMAGES_MINI.'/mini/'.$dir_name.'/'.$element);
 				}
 				if(!file_exists( DIR_IMAGES_MINI.'/web/'.$dir_name.'/'.$element))
 				{
-					resize($dir_name.'/'.$element, DIR_IMAGES_MINI.'/web/'.$dir_name.'/'.$element, 900);
+					$image->resize(1024,768);
+					$image->save(DIR_IMAGES_MINI.'/web/'.$dir_name.'/'.$element);
 				}
 			} 
 			else 
@@ -81,12 +90,12 @@ function resize($image, $target_file, $size) {
   $new_width = $new_height * $ratio;
 
   //move the file in the new location
-  move_uploaded_file($image['tmp_name'], $target_file);
+  move_uploaded_file($image, $target_file);
   
   // resample the image        
   $new_image = imagecreatetruecolor($new_width, $new_height);
-  $old_image = imagecreatefromjpeg($target_file);
-  imagecopyresampled($new_image,$old_image,0,0,0,0,$new_width, $new_height, $width, $height);        
+  $old_image = @imagecreatefromjpeg($image);
+  imagecopyresampled($new_image, $old_image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);        
 
   //output
   imagejpeg($new_image, $target_file, 100);
